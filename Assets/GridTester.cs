a5c8745cd5a841eb7967cbe0de3434fb1412faad
pseudo69 <mr.pseudo69@gmail.com>
2021-06-26 00:53:23 -0400

    enabled both old unity input system and new, created grid class and added utility package

---
+++ Assets/GridTester.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GridTester : MonoBehaviour
+{
+
+    private Grid grid;
+    
+   private void Start()
+    {
+        grid = new Grid(4, 2, 10f, new Vector3(20,0));
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
+}
