525bcd956e4729b7564ddd5358dc9d5a21c050d7
Francis <fmendez884@gmail.com>
2021-07-04 22:44:43 -0400

    players remains facing last direction inputted

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,14 +9,27 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rigidBody;
     private const float MOVEMENT_SPEED = 5f;
     private Vector2 moveDirection;
+    public Animator animator;
     
     public void onMovement(InputAction.CallbackContext context)
     {
         moveDirection = context.ReadValue<Vector2>();
+
     }
 
     private void FixedUpdate()
     {
         rigidBody.MovePosition(rigidBody.position + moveDirection * MOVEMENT_SPEED * Time.fixedDeltaTime);
+
+        animator.SetFloat("Horizontal", moveDirection.x);
+        animator.SetFloat("Vertical", moveDirection.y);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
+
+        if (moveDirection.x != 0 || moveDirection.y != 0)
+        {
+            animator.SetFloat("lastX", moveDirection.x);
+            animator.SetFloat("lastY", moveDirection.y);
+        }
+       
     }
 }
